d74e22e40caea1368d64d38690c4fc6575a5cab7
Matt Anderson <him@backattheranch.ca>
2025-09-11 11:14:18 -0700

    Header component and styles

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,28 @@
+import { MegaphoneIcon } from '@heroicons/react/24/solid';
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
+import styles from './header.module.css';
+
+export default function Header() {
+    return (
+        <header className="flex justify-between items-center px-10 pb-5 pt-4">
+            <div className="flex">
+                <h1 className="mr-8">
+                    <a href="/" className={`flex text-lg/1.3 font-extrabold ${styles.navlink}`}>
+                        <MegaphoneIcon className="size-6 mr-3" /> Connect
+                    </a>
+                </h1>
+                <nav>
+                    <ul className="flex gap-10">
+                        <li><a className={`text-sm ${styles.navlink}`} href="/members">Members</a></li>
+                        <li><a className={`text-sm ${styles.navlink}`} href="/notifications">Notifications</a></li>
+                        <li><a className={`text-sm ${styles.navlink}`} href="/messages">Messages</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <Avatar className="size-10">
+                <AvatarImage src="avatars/1.png" />
+                <AvatarFallback>MA</AvatarFallback>
+            </Avatar>
+        </header>
+    );
+}
