d6d04fbbd4628caed14b1524699050a667ad89e1
Violet Gibbs <violet.gibbs@student.kpu.ca>
2025-09-18 11:32:33 -0700

    no clue anymore

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { Separator } from '@/components/ui/separator';
 import './App.css';
+
 import Header from '@/components/header/Header';
 import LatestPosts from '@/components/latestPosts/Latestposts';
 
--- src/components/latestPosts/Latestposts.tsx
+++ src/components/latestPosts/Latestposts.tsx
@@ -3,6 +3,7 @@ import { PlusIcon } from '@heroicons/react/24/solid';
 import { Button } from '@/components/ui/button';
 import PostsList from '@/components/ui/posts/PostsList';
 import PostsSkeleton from '@/components/ui/skeletons/PostsSkeleton';
+import ErrorBoundary from '@/components/error/errorboundary'
 //import styles from './latestPosts.module.css';
 
 export default function LatestPosts() {
@@ -12,13 +13,15 @@ export default function LatestPosts() {
             <h2 className="text-4x1 font-bold mb-6">
                 Latest Posts
             </h2>
-            <Button className="">
+            <Button>
                 <PlusIcon className="size-6" /> Create Post
             </Button>
         </div>
+        <ErrorBoundary messaage="Failed to load posts">
         <Suspense fallback={<PostsSkeleton />}>
             <PostsList />
         </Suspense>
+        </ErrorBoundary>
     </main>
     );
 }
